baa74723de2be8d0427714929350c05746777af3
bucketfish0000 <piscibussitula@gmail.com>
2023-05-17 21:07:28 -0500

    lyapunov untested

--- qualification.py
+++ qualification.py
@@ -14,8 +14,20 @@ def learning_curve(model_list,test_length,data):
         loss_list.append(loss)
     return loss_list
 
-def lyapunov(model):
-    return
+def lyapunov_estimate(model,initial_in,tests = 100, delta_naught = 0.1, measure_time = 1000):
+    lamda = 0
+    dimension = model.d_m
+    model.training_time = 1
+    model.run_time = measure_time
+    test_data_0 = [initial_in]
+    out_list_0 = model.run_by_self(test_data_0)
+    for i in range(tests):
+        delta_in = delta_naught*np.random.rand(3)
+        test_data = [torch.add(initial_in,delta_in)]
+        out_list = model.run_by_self(test_data)
+        lamda += 1/measure_time * np.log(np.norm(np.divide(out_list[-1]-out_list_0[-1],delta_in)))
+    lamda /= tests
+    return lamda
 
 def config_space(model):
     KR,GR,MC=0,0,0
